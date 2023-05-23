52383e51780c2a0e7952a67149b1dd32b8784532
nuthairoo <nuthairoo@gmail.com>
2023-05-23 15:37:27 +0900

    change variable

--- src/app/api/distance/route.js
+++ src/app/api/distance/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    return new NextResponse("{distance: process.env.distance}":process.env.distance )
+    return new NextResponse("{distance: process.env.distance}": process.env.distance )
  
 }
