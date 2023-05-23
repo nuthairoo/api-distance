d478f7adfb8cd3a18d512e8fab54092b6b3f8a86
nuthairoo <nuthairoo@gmail.com>
2023-05-23 10:49:16 +0900

    feat:change update

--- src/app/api/distance/route.js
+++ src/app/api/distance/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    return new NextResponse("{message: hello world}")
+    return new NextResponse("{distance: 100}")
 }
