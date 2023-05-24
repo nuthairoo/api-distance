2b1da2face6f92a18c1003502284c4770f838260
nuthairoo <nuthairoo@gmail.com>
2023-05-24 09:08:54 +0900

    Update route.js

--- src/app/api/distance/route.js
+++ src/app/api/distance/route.js
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     //return new NextResponse("{distance: 100}" )
-    return new NextResponse("{process.env.distance}", process.env.distance )
+    return new NextResponse("{distance: ${process.env.distance}}" )
  
 }
