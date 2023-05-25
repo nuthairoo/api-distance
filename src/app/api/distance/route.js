e5212e8d02b2f3f99a0e98fb3664f6a945778716
nuthairoo <nuthairoo@gmail.com>
2023-05-25 13:10:09 +0900

    Update route.js

--- src/app/api/distance/route.js
+++ src/app/api/distance/route.js
@@ -1,12 +1,5 @@
 import { NextResponse } from "next/server";
 
-function Page() {
-  return <h1>The value of customKey is: {process.env.distance}</h1>;
+export async function GET() {
+    return new NextResponse("{distance: 100}" )
 }
- 
-export default Page;
-/*export async function GET() {
-    //return new NextResponse("{distance: 100}" )
-    return new NextResponse("{distance: ${process.env.distance}}" )
- 
-}*/
