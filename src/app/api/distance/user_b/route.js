1a6eff14dc4832096eebf9b0c6e8239cd38e4c63
nuthairoo <nuthairoo@gmail.com>
2023-05-26 09:31:02 +0900

    Update route.js

--- src/app/api/distance/user_b/route.js
+++ src/app/api/distance/user_b/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     //return new NextResponse("{distance: 2000}" )
-    return new NextResponse.json({
+    return NextResponse.json({
     distance: 2000, // ✅ POST function return type checks type of response body object
   });
 }
